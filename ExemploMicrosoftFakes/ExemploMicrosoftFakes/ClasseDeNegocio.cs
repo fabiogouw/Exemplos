ac6e6eb24df99c359447a230467b83f0cc8ae8e3
Fabio Gouw <fabiogouw@gmail.com>
2012-03-31 16:58:22 -0300

    Exemplo do post http://galorebr.blogspot.com.br/2012/03/novidades-no-visual-studio-11-beta.html

---
+++ ExemploMicrosoftFakes/ExemploMicrosoftFakes/ClasseDeNegocio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ExemploMicrosoftFakes
+{
+    public class ClasseDeNegocio
+    {
+        public void ExcluriArquivoAntigo(string arquivo)
+        {
+            // verifica se o arquivo existe
+            if (File.Exists(arquivo))
+            {
+                // verifica se ele é mais velho que 2 meses atrás (regra do sistema)
+                if (File.GetCreationTime(arquivo) < DateTime.Now.AddMonths(-2))
+                {
+                    // sendo assim, exclui
+                    File.Delete(arquivo);
+                }
+            }
+        }
+    }
+}
---
+++ ExemploMicrosoftFakes/ExemploMicrosoftFakes/TesteDeUnidade.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.QualityTools.Testing.Fakes; // para ShimsContext
+using System.IO.Fakes;  // para o ShimFile
+using System.Fakes; // para o ShimDateTime
+
+
+namespace ExemploMicrosoftFakes
+{
+    [TestClass]
+    public class TesteDeUnidade
+    {
+        [TestMethod]
+        public void ClasseDeNegocio_ConsultarConteudoArquivo_NaoDeveExcluirArquivoInexistente()
+        {
+            using (ShimsContext.Create())
+            {
+                // Arrange
+                bool excluido = false;
+                ShimFile.ExistsString = (s) => false;
+                ShimFile.DeleteString = (s) => excluido = true;
+                var alvo = new ClasseDeNegocio();
+                // Act
+                alvo.ExcluriArquivoAntigo("xxx");
+                // Assert
+                Assert.AreEqual(false, excluido);
+            }
+        }
+
+        [TestMethod]
+        public void ClasseDeNegocio_ConsultarConteudoArquivo_DeveExcluirArquivoMaisAntigoQue2MesesDaDataAtual()
+        {
+            using (ShimsContext.Create())
+            {
+                // Arrange
+                ShimDateTime.NowGet = () => DateTime.Parse("2011-04-01");
+                bool excluido = false;
+                ShimFile.ExistsString = (s) => true;
+                ShimFile.GetCreationTimeString = (s) => DateTime.Parse("2011-01-01");
+                ShimFile.DeleteString = (s) => excluido = true;
+                var alvo = new ClasseDeNegocio();
+                // Act
+                alvo.ExcluriArquivoAntigo("xxx");
+                // Assert
+                Assert.AreEqual(true, excluido);
+            }
+        }
+
+        [TestMethod]
+        public void ClasseDeNegocio_ConsultarConteudoArquivo_NaoDeveExcluirComAte2MesesDaDataAtual()
+        {
+            using (ShimsContext.Create())
+            {
+                // Arrange
+                ShimDateTime.NowGet = () => DateTime.Parse("2011-04-01");
+                bool excluido = false;
+                ShimFile.ExistsString = (s) => true;
+                ShimFile.GetCreationTimeString = (s) => DateTime.Parse("2011-03-01");
+                ShimFile.DeleteString = (s) => excluido = true;
+                var alvo = new ClasseDeNegocio();
+                // Act
+                alvo.ExcluriArquivoAntigo("xxx");
+                // Assert
+                Assert.AreEqual(false, excluido);
+            }
+        }
+    }
+}
